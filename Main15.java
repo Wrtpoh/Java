afcc1215719f4b3909f591fd88c51979bf44d811
Polina Chernova <polinaachernova22@gmail.com>
2025-10-04 10:50:50 +0500

    Update Main15.java

--- Main15.java
+++ Main15.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Main15 {
 
     // Задача 1: индекс первого вхождения числа в массиве
-    public static int findFirst(int[] arr, int x) {
+    public int findFirst(int[] arr, int x) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x) {
                 return i;
@@ -14,7 +14,7 @@ public class Main15 {
     }
 
     // Задача 2: максимальное по модулю значение в массиве
-    public static int maxAbs(int[] arr) {
+    public int maxAbs(int[] arr) {
         int max = Math.abs(arr[0]);
         for (int i = 1; i < arr.length; i++) {
             int absValue = Math.abs(arr[i]);
@@ -26,7 +26,7 @@ public class Main15 {
     }
 
     // Задача 3: вставка массива ins в arr на позицию pos
-    public static int[] add(int[] arr, int[] ins, int pos) {
+    public int[] add(int[] arr, int[] ins, int pos) {
         int[] result = new int[arr.length + ins.length];
         for (int i = 0; i < pos; i++) {
             result[i] = arr[i];
@@ -41,7 +41,7 @@ public class Main15 {
     }
 
     // Задача 4: переворот массива
-    public static int[] reverseBack(int[] arr) {
+    public int[] reverseBack(int[] arr) {
         int[] reversed = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             reversed[i] = arr[arr.length - 1 - i];
@@ -50,7 +50,7 @@ public class Main15 {
     }
 
     // Задача 5: индексы всех вхождений x в массив
-    public static int[] findAll(int[] arr, int x) {
+    public int[] findAll(int[] arr, int x) {
         int count = 0;
         for (int value : arr) {
             if (value == x) {
@@ -73,6 +73,7 @@ public class Main15 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        Main15 obj = new Main15();
 
         System.out.println("Введите размер массива:");
         int n16 = sc.nextInt();
@@ -81,14 +82,14 @@ public class Main15 {
         for (int i = 0; i < n16; i++) arr16[i] = sc.nextInt();
         System.out.print("Введите число для поиска: ");
         int x16 = sc.nextInt();
-        System.out.println("Поиск по значению" + findFirst(arr16, x16)); //{1, 2, 3, 4, 2, 2, 5}
+        System.out.println("Поиск по значению" + obj.findFirst(arr16, x16)); //{1, 2, 3, 4, 2, 2, 5}
 
         System.out.print("Введите размер массива: ");
         int n17 = sc.nextInt();
         int[] arr17 = new int[n17];
         System.out.println("Введите элементы массива:");
         for (int i = 0; i < n17; i++) arr17[i] = sc.nextInt();
-        System.out.println("Максимум по модулю: " + maxAbs(arr17));
+        System.out.println("Максимум по модулю: " + obj.maxAbs(arr17));
 
         System.out.print("Введите размер массива: ");
         int n18 = sc.nextInt();
@@ -102,14 +103,14 @@ public class Main15 {
         for (int i = 0; i < m; i++) ins[i] = sc.nextInt();
         System.out.print("Введите позицию вставки: ");
         int pos = sc.nextInt();
-        System.out.println("Результат: " + Arrays.toString(add(arr18, ins, pos)));
+        System.out.println("Результат: " + Arrays.toString(obj.add(arr18, ins, pos)));
 
         System.out.print("Введите размер массива: ");
         int n19 = sc.nextInt();
         int[] arr19 = new int[n19];
         System.out.println("Введите элементы массива:");
         for (int i = 0; i < n19; i++) arr19[i] = sc.nextInt();
-        System.out.println("Перевёрнутый массив: " + Arrays.toString(reverseBack(arr19)));
+        System.out.println("Перевёрнутый массив: " + Arrays.toString(obj.reverseBack(arr19)));
 
         System.out.print("Введите размер массива: ");
         int n20 = sc.nextInt();
@@ -118,6 +119,6 @@ public class Main15 {
         for (int i = 0; i < n20; i++) arr20[i] = sc.nextInt();
         System.out.print("Введите число для поиска: ");
         int x20 = sc.nextInt();
-        System.out.println("Все вхождения числа: " + Arrays.toString(findAll(arr20, x20)));
+        System.out.println("Все вхождения числа: " + Arrays.toString(obj.findAll(arr20, x20)));
     }
 }
